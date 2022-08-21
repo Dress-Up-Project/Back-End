cb204499dcb8f531cc7dc2b865d342163e9016ac
BasmaEsmail <basmaabas755@gmail.com>
2022-08-21 16:33:53 +0200

    cloth class

--- Dress-Up/Dress-Up-Models/Clothes/Clothes.cs
+++ Dress-Up/Dress-Up-Models/Clothes/Clothes.cs
@@ -8,5 +8,21 @@ namespace Dress_Up_Models
 {
     public class Clothes
     {
+        public int ID { get; set; }
+        public string? NameEn { get; set; }
+        public string? NameAr { get; set; }
+        public float Price { get; set; }
+        public string? Details { get; set; }
+        public int SizeNum { get; set; }
+        public string? Size { get; set; }
+        public bool Gender { get; set; }
+        public string? Color { get; set; }
+        public string? ImageUrl { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public int CategoryID { get; set; }
+
+        //public Category Category {get;set;} 
     }
 }
